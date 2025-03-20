ff64cb16105dadbe10aa1ee8590a46c42c6cedc7
Davide Piccinini <davide.piccinini.95@gmail.com>
2025-03-20 17:41:08 +0100

    Update powerhsell profile with posh-git style

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,14 @@
 Import-Module posh-git
+
+function Set-PoshGitStyle {
+    $GitPromptSettings.DefaultPromptPrefix.Text = '$(Get-Date -f "MM-dd HH:mm:ss") '
+    $GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::DarkGray
+
+    $GitPromptSettings.DefaultPromptSuffix.ForegroundColor = '#967bb6'
+
+    $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
+    $GitPromptSettings.DefaultPromptPath.ForegroundColor = '#967bb6'
+    $GitPromptSettings.DefaultPromptWriteStatusFirst = $false
+}
+
+Set-PoshGitStyle
